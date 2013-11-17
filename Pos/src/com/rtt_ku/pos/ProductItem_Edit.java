7a4fe9a02c297f63ffefebcca56113dd13e38f9d
KokKok <kok_satan@hotmail.com>
2013-11-18 00:40:27 +0700

    add edit function on product tab

--- Pos/src/com/rtt_ku/pos/ProductItem_Edit.java
+++ Pos/src/com/rtt_ku/pos/ProductItem_Edit.java
@@ -1,5 +1,9 @@
 package com.rtt_ku.pos;
 
+import com.database.pos.Database;
+import com.rtt_store.pos.StoreController;
+
+import Inventory.Product;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,29 +11,46 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class ProductItem_Edit extends Activity{
-	
+
+    public static StoreController sCT;
+    private Button okButton;
+    private Button cancelButton; 
+    private EditText id;
+    private EditText name;
+    private EditText cost;
+    private EditText price;
+    
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.product_edit_item);
 		
-		EditText id = (EditText)findViewById(R.id.product_id_editText);
-		EditText name = (EditText)findViewById(R.id.product_name_editText);
-		EditText quantity = (EditText)findViewById(R.id.product_quantity_editText);
-		EditText cost = (EditText)findViewById(R.id.product_cost_editText);
-		EditText price = (EditText)findViewById(R.id.product_price_editText);
+		// call Store Controller
+		Database myDb = new Database(this);
+		myDb.getWritableDatabase();
+		sCT = new StoreController(myDb);
 		
-		Button okButton = (Button)findViewById(R.id.product_info_okButton);
-		Button cancelButton = (Button)findViewById(R.id.product_info_cancelButton);
+		Intent intend = getIntent();
+		final String product_code = intend.getExtras().getString("pc");
+		final Product p = sCT.getProduct(product_code);
 		
+		okButton = (Button)findViewById(R.id.product_info_okButton);
+		cancelButton = (Button)findViewById(R.id.product_info_cancelButton);
+		id = (EditText)findViewById(R.id.product_id_editText);
+		name = (EditText)findViewById(R.id.product_name_editText);
+		cost = (EditText)findViewById(R.id.product_cost_editText);
+		price = (EditText)findViewById(R.id.product_price_editText);
+		
+		setEditText(p);
+			
 		okButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
 			}
 		});
 		
@@ -38,8 +59,18 @@ public class ProductItem_Edit extends Activity{
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				startActivity(new Intent(v.getContext(), ProductItem_Info.class));
+				Intent intent = new Intent(v.getContext(), ProductItem_Info.class);
+				intent.putExtra("pc", p.getProduct_Code());
+				startActivity(intent);
 			}
 		});
+	}
+	
+	private void setEditText(Product p){
+		id.setText(p.getProduct_Code());
+		id.setEnabled(false);
+		name.setText(p.getName());
+//		cost.setText(p.getCost());
+		price.setText(p.getPrice()+"");
 	}
 }
--- Pos/src/com/rtt_ku/pos/ProductItem_Info.java
+++ Pos/src/com/rtt_ku/pos/ProductItem_Info.java
@@ -1,14 +1,22 @@
 package com.rtt_ku.pos;
 
+import com.database.pos.Database;
+import com.rtt_store.pos.StoreController;
+
+import Inventory.Product;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class ProductItem_Info extends Activity{
+public class ProductItem_Info extends Activity {
 
+    public static StoreController sCT;
+    private Product p; 
+    
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.product_iteminfo);
@@ -16,12 +24,32 @@ public class ProductItem_Info extends Activity{
 		Button cancelButton = (Button)findViewById(R.id.productItem_cancel_button);
 		Button editButton = (Button)findViewById(R.id.productItem_edit_button);
 		
+		// call Store Controller
+		Database myDb = new Database(this);
+		myDb.getWritableDatabase();
+		sCT = new StoreController(myDb);
+			
+		Intent intent = getIntent();
+		String product_code = intent.getExtras().getString("pc");
+		p = sCT.getProduct(product_code);
+				
+				
+		// view matching
+		TextView id = (TextView)findViewById(R.id.product_itemId);
+		TextView name = (TextView)findViewById(R.id.product_item_name);
+		TextView cost = (TextView)findViewById(R.id.product_itemCost);
+		TextView price = (TextView)findViewById(R.id.product_itemPrice);
+				
+		setTextView(id,name,cost,price,p);
+			
 		editButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				startActivity(new Intent(v.getContext(), ProductItem_Edit.class));
+				Intent intent = new Intent(v.getContext(), ProductItem_Edit.class);
+				intent.putExtra("pc", p.getProduct_Code());
+				startActivity(intent);
 			}
 		});
 		
@@ -30,8 +58,16 @@ public class ProductItem_Info extends Activity{
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				
 				startActivity(new Intent(v.getContext(), main_activity.class));
 			}
 		});
+	}
+
+	private void setTextView(TextView id, TextView name, TextView cost,TextView price, Product p) {
+		id.setText(p.getProduct_Code());
+		name.setText(p.getName());
+		// cost.setText(p.getCost());
+		price.setText(p.getPrice()+"");
 	}
 }
