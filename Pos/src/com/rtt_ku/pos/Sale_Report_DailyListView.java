a7ffb57414de58ea20af13314505cb9ebec3996d
bik4412 <bik4412@hotmail.com>
2013-12-06 22:13:06 +0700

    Edit sale_report_daily

--- Pos/src/com/rtt_ku/pos/Sale_Report_DailyListView.java
+++ Pos/src/com/rtt_ku/pos/Sale_Report_DailyListView.java
@@ -1,15 +1,27 @@
 package com.rtt_ku.pos;
 
+import com.database.pos.Database;
+import com.rtt_store.pos.StoreController;
+import com.salerecord.pos.DatabaseSaleRecord;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ListView;
 
 public class Sale_Report_DailyListView extends Activity {
-
+	public static ListView listView;
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sale_report_daily_listview);
+		listView = (ListView)findViewById(R.id.sale_report_listView);
+		Database myDb = new Database(this);
+        myDb.getReadableDatabase();
+        DatabaseSaleRecord DbSr = new DatabaseSaleRecord(this);
+        DbSr.getReadableDatabase();
+		StoreController sCT = new StoreController(myDb,DbSr);
+
 	}
 
 	public View getView() {
